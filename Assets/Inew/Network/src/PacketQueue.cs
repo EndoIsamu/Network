5ab4a2ea8052cedcfdab850fee9acbc80d75abd0
EndoIsamu <alupaca1363@gmail.com>
2019-02-21 00:12:20 +0900

    PacketQueueをスレッドセーフに

--- Assets/Inew/Network/src/PacketQueue.cs
+++ Assets/Inew/Network/src/PacketQueue.cs
@@ -17,11 +17,10 @@ namespace Inew.Network
 
         //
         private MemoryStream m_streamBuffer;
-
         private List<PacketInfo> m_offsetList;
-
         private int m_offset = 0;
 
+        private readonly object _lockObj = new object();
 
         // 
         public PacketQueue()
@@ -38,44 +37,51 @@ namespace Inew.Network
             info.offset = m_offset;
             info.size = size;
 
-            // パケット格納情報を保存.
-            m_offsetList.Add(info);
+            lock (_lockObj)
+            {
+                // パケット格納情報を保存.
+                m_offsetList.Add(info);
 
-            // パケットデータを保存.
-            m_streamBuffer.Position = m_offset;
-            m_streamBuffer.Write(data, 0, size);
-            m_streamBuffer.Flush();
-            m_offset += size;
+                // パケットデータを保存.
+                m_streamBuffer.Position = m_offset;
+                m_streamBuffer.Write(data, 0, size);
+                m_streamBuffer.Flush();
+                m_offset += size;
+            }
 
             return size;
         }
 
         public int Dequeue(ref byte[] buffer, int size)
         {
-
             if (m_offsetList.Count <= 0)
             {
                 return -1;
             }
 
-            PacketInfo info = m_offsetList[0];
+            int recvSize = 0;
 
-            // バッファから該当するパケットデータを取得する.
-            int dataSize = Math.Min(size, info.size);
-            m_streamBuffer.Position = info.offset;
-            int recvSize = m_streamBuffer.Read(buffer, 0, dataSize);
-
-            // キューデータを取り出したので先頭要素を削除.
-            if (recvSize > 0)
+            lock (_lockObj)
             {
-                m_offsetList.RemoveAt(0);
-            }
+                PacketInfo info = m_offsetList[0];
 
-            // すべてのキューデータを取り出したときはストリームをクリアしてメモリを節約する.
-            if (m_offsetList.Count == 0)
-            {
-                Clear();
-                m_offset = 0;
+                // バッファから該当するパケットデータを取得する.
+                int dataSize = Math.Min(size, info.size);
+                m_streamBuffer.Position = info.offset;
+                recvSize = m_streamBuffer.Read(buffer, 0, dataSize);
+
+                // キューデータを取り出したので先頭要素を削除.
+                if (recvSize > 0)
+                {
+                    m_offsetList.RemoveAt(0);
+                }
+
+                // すべてのキューデータを取り出したときはストリームをクリアしてメモリを節約する.
+                if (m_offsetList.Count == 0)
+                {
+                    Clear();
+                    m_offset = 0;
+                }
             }
 
             return recvSize;
